3dec027e5a1962a4804536df65711c81141dd45f
todooou-mf <todo.erii@moneyforward.co.jp>
2024-07-24 22:17:40 +0900

    fix todo

--- Todo/Day5/Hello.java
+++
@@ -1,7 +0,0 @@
-package Day5;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
---
+++ Todo/day6/Main.java
@@ -0,0 +1,14 @@
+package day6;
+
+public class Main {
+    public static void main(String[] args){
+         elself();
+    }
+
+    public static void elself() {
+        int t = 31;
+        if (t > 30) System.out.println("cooler");
+        else if (t < 20) System.out.println("heater");
+        else System.out.println("off"); 
+    }
+}
